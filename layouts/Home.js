bd105d3ef63780b17392e0cc20b65b8b38173290
Gustavo Franco <gustavo.franco@solution4fleet.com.br>
2022-11-07 00:21:04 -0300

    first commit

--- layouts/Home.js
+++ layouts/Home.js
@@ -1,10 +1,43 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native'
+import styled from 'styled-components'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 export function Home() {
     return (
-        <View>
-            <Text>Gustavo</Text>
-        </View>
+        <Content>
+            <Header resizeMode="contain" resizeMethod="auto" source={require('../assets/header.png')} />
+            <Center>
+                <Mid resizeMode="contain" resizeMethod="auto" source={require('../assets/busy-working-woman.jpg')} />
+            </Center>
+            <Footer resizeMode="contain" resizeMethod="auto" source={require('../assets/next1.png')} />
+        </Content>
     )
 }
+
+const Content = styled(View)`
+    flex: 1;
+    justify-content: space-between;
+    width: 100%;
+    height: auto;
+    align-items: center;
+    background-color: white;
+`
+const Header = styled(Image)`
+    margin-top: -57.5%;
+    width: 100%;
+`
+
+const Center = styled(View)`
+    margin-top: -60%;
+    flex: 1;
+    width: 100%;
+`
+
+const Mid = styled(Image)`
+    width: 100%;
+`
+
+const Footer = styled(Image)`
+    margin-top: 75%;
+    width: 100%;
+`
